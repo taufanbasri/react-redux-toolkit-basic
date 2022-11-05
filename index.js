8589ffc11ab882e7aa7ad7ed203f9fc32c1f5478
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2022-11-05 15:08:14 +0700

    Ice Cream Feature

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const store = require("./app/store");
 const cakeActions = require("./features/cake/cakeSlice").cakeActions;
+const iceCreamActions =
+  require("./features/iceCream/iceCreamSlice").iceCreamActions;
 
 console.log("Initial state: ", store.getState());
 
@@ -11,5 +13,10 @@ store.dispatch(cakeActions.ordered());
 store.dispatch(cakeActions.ordered());
 store.dispatch(cakeActions.ordered());
 store.dispatch(cakeActions.restocked(10));
+
+store.dispatch(iceCreamActions.ordered());
+store.dispatch(iceCreamActions.ordered());
+store.dispatch(iceCreamActions.ordered());
+store.dispatch(iceCreamActions.restocked(3));
 
 unsubscribe();
