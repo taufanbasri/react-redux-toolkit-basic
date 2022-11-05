fa69a630510ba8d00b57c9d9f835436338ea77d5
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2022-11-05 15:55:48 +0700

    Extra Reducer

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const cakeActions = require("./features/cake/cakeSlice").cakeActions;
 const iceCreamActions =
   require("./features/iceCream/iceCreamSlice").iceCreamActions;
 
-const unsubscribe = store.subscribe(() => {});
+console.log("Initial state: ", store.getState());
+
+const unsubscribe = store.subscribe(() => {
+  console.log("Updated state: ", store.getState());
+});
 
 store.dispatch(cakeActions.ordered());
 store.dispatch(cakeActions.ordered());
