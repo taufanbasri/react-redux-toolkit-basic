5e92dda4b2223221b5c55539a28b467ef1096571
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2022-11-05 18:47:54 +0700

    Async Thunk

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const store = require("./app/store");
 const cakeActions = require("./features/cake/cakeSlice").cakeActions;
 const iceCreamActions =
   require("./features/iceCream/iceCreamSlice").iceCreamActions;
+const fetchUsers = require("./features/user/userSlice").fetchUsers;
 
 console.log("Initial state: ", store.getState());
 
@@ -9,14 +10,16 @@ const unsubscribe = store.subscribe(() => {
   console.log("Updated state: ", store.getState());
 });
 
-store.dispatch(cakeActions.ordered());
-store.dispatch(cakeActions.ordered());
-store.dispatch(cakeActions.ordered());
-store.dispatch(cakeActions.restocked(10));
+store.dispatch(fetchUsers());
 
-store.dispatch(iceCreamActions.ordered());
-store.dispatch(iceCreamActions.ordered());
-store.dispatch(iceCreamActions.ordered());
-store.dispatch(iceCreamActions.restocked(3));
+// store.dispatch(cakeActions.ordered());
+// store.dispatch(cakeActions.ordered());
+// store.dispatch(cakeActions.ordered());
+// store.dispatch(cakeActions.restocked(10));
 
-unsubscribe();
+// store.dispatch(iceCreamActions.ordered());
+// store.dispatch(iceCreamActions.ordered());
+// store.dispatch(iceCreamActions.ordered());
+// store.dispatch(iceCreamActions.restocked(3));
+
+// unsubscribe();
