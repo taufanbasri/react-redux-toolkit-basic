57124469b03e3d7a29e3963dd70bae8d141403c4
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2022-11-05 15:14:18 +0700

    Logger Middleware

--- index.js
+++ index.js
@@ -3,11 +3,7 @@ const cakeActions = require("./features/cake/cakeSlice").cakeActions;
 const iceCreamActions =
   require("./features/iceCream/iceCreamSlice").iceCreamActions;
 
-console.log("Initial state: ", store.getState());
-
-const unsubscribe = store.subscribe(() => {
-  console.log("Updated state: ", store.getState());
-});
+const unsubscribe = store.subscribe(() => {});
 
 store.dispatch(cakeActions.ordered());
 store.dispatch(cakeActions.ordered());
